5a33cd692eb576d6ea7bb44ff3cfc8f73fec0670
surendra-177 <2surendranath@gmail.com>
2025-08-20 17:43:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -58,4 +58,8 @@ app.post("/create-order", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("✅ Backend is running on Render!");
+});
+
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
