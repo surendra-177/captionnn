8941538ea6de58febdb58980766afce3cdf4f144
surendra-177 <2surendranath@gmail.com>
2025-08-20 17:24:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,5 +57,5 @@ app.post("/create-order", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 0.0.0.0;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
